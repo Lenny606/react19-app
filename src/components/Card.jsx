70bd3399987c29fdd855e616110afd0ad6549521
Tomas Kravcik <kravcik@procorp.cz>
2025-01-25 22:05:37 +0100

    hooks

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,30 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {HiHeart, HiOutlineHeart} from "react-icons/hi";
 
 export const Card = ({title, description}) => {
     const [like, setLike] = useState(false)
+    const [clicks, setClicks] = useState(0)
 
     function likeHandler(like) {
-        setLike(!like)
+        setLike((prevState) => !prevState)
+        clickHandler()
     }
 
+    function clickHandler() {
+        setClicks((prevState) => prevState + 1)
+    }
+
+    useEffect(() => {
+
+    }, []);
+
     return (
-        <div className="bg-white rounded-lg shadow-md overflow-hidden">
+        <div className="bg-white rounded-lg shadow-md overflow-hidden"
+             >
             <div className="p-6">
                 <h2 className="text-xl font-semibold mb-2">{title}</h2>
                 <p className="text-gray-600 mb-4">{description}</p>
+
                 <button
                     onClick={() => likeHandler(like)}
                     className={`px-4 py-2 rounded-md transition-colors ${
@@ -23,6 +35,7 @@ export const Card = ({title, description}) => {
                 >
                     {like ? <HiHeart className="text-2xl"/> : <HiOutlineHeart className="text-2xl"/>}
                 </button>
+                <p className="text-gray-600 mt-4">{clicks ? `Number of clicks: ${clicks}` : null}</p>
             </div>
         </div>
     )
